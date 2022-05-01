ddf9fdb99ba07b404f76ede7a54be09373035bec
kanm4s <kanm4s@gmail.com>
2022-05-01 23:38:25 +0700

    template login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,25 @@
+import { useState } from "react";
 import "./Login.css";
 
 export default function Login() {
+    const [signUpPage, setSignUpPage] = useState(false);
+
+    const handleSignUpPage = () => {
+        setSignUpPage(!signUpPage);
+    };
     return (
         <div className="Login-master-container">
             <div className="login-container">
-                <div className="signup-container">
-                    <div className="signup-element">
-                        <h1 className="text-white header">
-                            Welcome To Kantar ERP
-                        </h1>
-                        <p className="signup-description text-white">
-                            The best erp ever existed in the world
-                        </p>
-                        <button className="d-btn white-bd">
-                            <h1 className="text-white">Sign up</h1>
-                        </button>
-                    </div>
-                </div>
-
-                <form className="form-container">
-                    <div className="signin-element">
+                <form
+                    className={`form-signup-container ${
+                        signUpPage ? "form-signup-container-show" : ""
+                    }`}
+                >
+                    <div
+                        className={`signin-element ${
+                            signUpPage ? "signin-element-hidden" : ""
+                        }`}
+                    >
                         <h1 className="text-main-color header">
                             Sign in to ERP
                         </h1>
@@ -33,6 +33,57 @@ export default function Login() {
                             type="password"
                             placeholder="Password"
                         />
+                        <button className="d-btn primary-bd">
+                            <h1 className="text-white">Log in</h1>
+                        </button>
+                        <p className="forget-password">Forget password</p>
+                    </div>
+                </form>
+                {/* signup-element-hidden */}
+                <div className="signup-container">
+                    <div className={`signup-element ${signUpPage ? "" : ""}`}>
+                        <h1 className="text-white header">
+                            Welcome To Kantar ERP
+                        </h1>
+                        <p className="signup-description text-white">
+                            The best erp ever existed in the world
+                        </p>
+                        <button
+                            className="d-btn white-bd"
+                            onClick={handleSignUpPage}
+                        >
+                            <h1 className="text-white">Sign up</h1>
+                        </button>
+                    </div>
+                </div>
+
+                <form
+                    className={`form-container ${
+                        signUpPage ? "form-container-hidden" : ""
+                    } `}
+                >
+                    <div
+                        className={`signin-element ${
+                            signUpPage ? "signin-element-hidden" : ""
+                        }`}
+                    >
+                        <h1 className="text-main-color header">
+                            Sign in to ERP
+                        </h1>
+                        <input
+                            className="input-login"
+                            type="text"
+                            placeholder="ID"
+                        />
+                        <input
+                            className="input-login"
+                            type="password"
+                            placeholder="Password"
+                        />
+                        <button className="d-btn primary-bd">
+                            <h1 className="text-white">Log in</h1>
+                        </button>
+                        <p className="forget-password">Forget password</p>
                     </div>
                 </form>
             </div>
